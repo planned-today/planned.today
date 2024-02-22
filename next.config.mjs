5650d828b7bb7d96baf03c6b8f0abdcfa7453aac
paraswtf <parasbpndeshpande@gmail.com>
2024-02-22 11:14:51 +0530

    Update redirects for github and docs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const githubUrl = "https://github.com/planned-today/planned.today";
+const nextConfig = {
+	async redirects() {
+		return [
+			{
+				source: "/github",
+				destination: githubUrl,
+				permanent: true
+			},
+			{
+				source: "/docs",
+				destination: "https://docs.planned.today",
+				permanent: true
+			}
+		];
+	}
+};
 
 export default nextConfig;

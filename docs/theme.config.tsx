e473bbcfcfc521135753d125f2d237d4203250db
Pranav Praveen <pranavpraveen0@gmail.com>
2024-02-22 13:57:13 +0530

    Title Update

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -1,16 +1,37 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
-import Image from 'next/image'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import Image from "next/image";
+import { useRouter } from "next/router";
 
 const config: DocsThemeConfig = {
-  logo: <span> <Image src="/assets/head-logo-icon.svg" width="200" height="80" alt="Planned.today" /> </span>,
-  project: {
-    link: 'https://github.com/paraswtf/planned.today/',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Planned.today',
-  },
-}
+	logo: (
+		<span>
+			{" "}
+			<Image
+				src="/assets/head-logo-icon.svg"
+				width="200"
+				height="80"
+				alt="Planned.today"
+			/>{" "}
+		</span>
+	),
+	project: {
+		link: "https://github.com/paraswtf/planned.today/"
+	},
+	docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
+	footer: {
+		text: "Planned.today"
+	}
+};
 
-export default config
+export default {
+	config,
+	useNextSeoProps() {
+		const { asPath } = useRouter();
+		if (asPath !== "/") {
+			return {
+				titleTemplate: "%s | Planned.today"
+			};
+		}
+	}
+};
